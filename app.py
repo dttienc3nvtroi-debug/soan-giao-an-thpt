eb9d3a917abef280322e5ce034e4925d4f248128
dttienc3nvtroi-debug <dttien.c3nvtroi@khanhhoa.edu.vn>
2026-08-03 21:37:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ with st.sidebar:
     
     model_name = st.selectbox(
         "Mô hình AI xử lý:",
-        ["gemini-1.5-flash", "gemini-2.0-flash", "gemini-1.5-pro"],
+        ["gemini-3.6-flash", "gemini-3.6-flash", "gemini-3.6-flash"],
         index=0,
         help="Nên chọn gemini-1.5-flash để đọc file PDF/Ảnh nhanh và mượt nhất"
     )
